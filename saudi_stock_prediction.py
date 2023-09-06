94328ffd5a32680fa39399ecdac85a22000b847b
flaatah <staralflaatah@hotmail.com>
2023-09-06 23:26:39 +0300

    Update saudi_stock_prediction.py

--- saudi_stock_prediction.py
+++ saudi_stock_prediction.py
@@ -72,7 +72,7 @@ train = train.rename(columns={"Date": "ds", "Close": "y"})
 train['ds'] = pd.to_datetime(train['ds'])
 
 #Fit and predict
-m = Prophet(yearly_seasonality=True)
+m = Prophet()
 m.add_country_holidays(country_name='SA')
 m.fit(train)
 future = m.make_future_dataframe(periods=period, freq="M")
